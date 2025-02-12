52ae9d7ebdfee3edbc12ca032941736e4c967060
timots <timothydoli639k@gmail.com>
2025-02-12 11:17:39 +0700

    update

--- src/pages/api/v1/blogArticle/comment-post/index.js
+++ src/pages/api/v1/blogArticle/comment-post/index.js
@@ -16,8 +16,7 @@ export default async function handler(req, res) {
     return res.status(405).json({ message: 'Method Not Allowed' });
   }
 
-  // const baseUrl = 'https://deoapp.com/api/public/platform/laju-peduli/blog';
-  const baseUrl = 'http://172.16.20.158:3000/api/public/platform/laju-peduli/blog-sub';
+  const baseUrl = 'https://deoapp.com/api/public/platform/laju-peduli/blog-sub';
 
   const requestData = req.body;
 
